e23e0b7682a07790a30b6793297cd9ae7733f4a3
olegsinyavskiy <olegsinyavskiy@gmail.com>
2015-06-11 10:19:33 -0700

    fix anonymous user

--- src/main/java/com/jdriven/stateless/security/UserController.java
+++ src/main/java/com/jdriven/stateless/security/UserController.java
@@ -1,10 +1,12 @@
 package com.jdriven.stateless.security;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class UserController {
@@ -12,11 +14,12 @@ public class UserController {
 	@RequestMapping(value = "/api/user/current", method = RequestMethod.GET)
 	public User getCurrent() {
 		final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		User user= (User)(authentication.getPrincipal());
-		if (user == null)
+		if (authentication instanceof AnonymousAuthenticationToken)
 		{
-		    user = new User(); //anonymous user support 
+		    //anonymous user support 
+            return new User();
+		} else {
+		    return (User)(authentication.getPrincipal());
 		}
-		return user;
 	}
 }
